51680696c0e266e2501e229f66df50c428ab0bf2
Sajad Abbasi <s.abbasi@me.com>
2024-12-04 19:57:49 +0330

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,7 @@
+import test, { expect } from "@playwright/test";
+
+test.describe('Sample', () => {
+  test('TA-001:As a user, I must be able to login with my correct credentials and be redirected to list page', async () => {
+    expect(1).toBe(1);
+  });
+});
